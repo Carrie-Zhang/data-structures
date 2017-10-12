7d458ab24b27780a5cfa773d1232b016033030cf
Hack Reactor Student <student@hackreactor.com>
2017-10-12 16:07:07 -0700

    first edit linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,15 +2,39 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.count = 0;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    // list[value] = node;
+    list.tail = node;
+    if (list.count === 0) {
+      list.head = node;
+    } else {
+      list.head.next = node;
+    }
+    list.count ++;
   };
 
   list.removeHead = function() {
+    var temp = list.head;
+    delete list[list.head.value];
+    list.head = list.head.next;
+    list.count --;
+    return temp.value;
   };
 
   list.contains = function(target) {
-  };
+    var output = false;
+    var node = list.head;
+    while (node) {
+      if (node.value === target) {
+        output = true;
+      }
+      node = node.next;
+    }
+    return output;
+  }; 
 
   return list;
 };

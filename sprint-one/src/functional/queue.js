a7f7f58a38d3febf692c6878fca54e90bc68a989
Hack Reactor Student <student@hackreactor.com>
2017-10-11 17:07:42 -0700

    first commit up to functional-shared stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var tempVal;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.dequeue = function() {
+    tempVal = storage[0];
+    count --;
+    if (count < 0) {
+      count = 0;
+    }
+    delete someInstance[0];
+    for (var i = 0; i < count; i++) {
+      storage[i] = storage[i + 1];
+    }
+    delete storage[count];
+    return tempVal;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var tempVal;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.pop = function() {
+    count --;
+    if (count < 0) {
+      count = 0;
+    }
+    tempVal = storage[count];
+    delete storage[count];
+
+    return tempVal;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;

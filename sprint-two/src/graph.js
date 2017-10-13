ce426cb2967fa3a1b0874e23d68567c8586e448a
Hack Reactor Student <student@hackreactor.com>
2017-10-13 10:47:09 -0700

    upto graph removeNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,31 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === node) {
+      this.storage.splice(i, 1);
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -32,6 +45,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 Graph.prototype.forEachNode = function(cb) {
 };
 
+var graph = new Graph();
 /*
  * Complexity: What is the time complexity of the above functions?
  */

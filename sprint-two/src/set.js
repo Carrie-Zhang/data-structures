a5ddfaeed4f1794de8e2adaf867ca622aec3e2f1
Hack Reactor Student <student@hackreactor.com>
2017-10-13 11:47:40 -0700

    finsih upto set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if (index !== -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * O(n)
  */

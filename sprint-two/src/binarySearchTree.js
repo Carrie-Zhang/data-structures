37886886b75e706e52a6e7d377910d2e4eef0d2d
Carrie Zhang <carrie@Carries-MacBook-Pro.local>
2017-10-14 11:21:27 -0700

    complete binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,12 +1,11 @@
 var BinarySearchTree = function(value) {
   var newBinaryTree = {};
+  
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+  newBinaryTree.value = value;
+ 
   newBinaryTree = _.extend(newBinaryTree, binaryTreeMethods);
-  this.children = []; // 0/1/2 children
-  this.left = this.children[0];
-  this.right = this.children[1];
-  this.value = value;
-  this.numOfChildren = 0;
-  // this.midPoint = value;
   
   return newBinaryTree;
 };
@@ -14,33 +13,57 @@ var BinarySearchTree = function(value) {
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(value) {
-//
-  if (value < this.value) {
-    // if (this.children.length === 0) {
-    this.left = BinarySearchTree(value);
-    this.children[0] = this.left;
-    this.numOfChildren ++;
-    // } 
-  } 
-  if (value > this.value) {
-    // if (this.children.length === 0) {
-    this.right = BinarySearchTree(value);
-    this.children[1] = this.right;
-    this.numOfChildren ++;
-    // }
+  var node = BinarySearchTree(value);
+  var currentNode = this;
+
+  if (value < currentNode.value) {
+    if (currentNode.left === null) {
+      currentNode.left = node;
+    } else {
+      currentNode.left.insert(value);
+    }
   }
-  // if (this.children.length > 0) {
-  for (var i = 0; i < this.numOfChildren; i++) {
-    this.childern[i].insert(value);
-  }
-  // for (var i = 0; i < this.children.length; i++) {
-  //   this.children[i].insert(value);
-  // }
+  if (value > currentNode.value) {
+    if (currentNode.right === null) {
+      currentNode.right = node;
+    } else {
+      currentNode.right.insert(value);
+    }
+ }
 };
-binaryTreeMethods.contains = function() {};
-binaryTreeMethods.depthFirstLog = function() {};
+binaryTreeMethods.contains = function(value) {
+  var currentNode = this;
+
+  if (value === currentNode.value) {
+    return true;
+  }
+  if (value < currentNode.value) {
+    if (currentNode.left !== null) {
+      return currentNode.left.contains(value);
+    }
+  }
+  if (value > currentNode.value) {
+    if (currentNode.right !== null) {
+      return currentNode.right.contains(value);
+    } 
+  }
+  return false;
+};
+binaryTreeMethods.depthFirstLog = function(callback) {
+  var currentNode = this;
+  callback(currentNode.value);
+  if (currentNode.left !== null) {
+    currentNode.left.depthFirstLog(callback);
+  } 
+  if (currentNode.right !== null) {
+    currentNode.right.depthFirstLog(callback);
+  }
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: O(logn)
+ * contains: O(logn)
+ * depthFirstLog: O(n)
  */
